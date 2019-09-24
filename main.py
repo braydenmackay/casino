d7d9e9ee9c0aec75d81dc1cb8d588943b215d11a
braydenmackay <braydenmackay1@gmail.com>
2019-09-23 19:37:15 -0600

    revised and updated. added check wallet function

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import random
 
-my_wallet = [20]
+my_wallet = [3]
 
 def greeting():
   print(f"Welcome to the Casino!\nYou have been given a ${sum(my_wallet):.2f} credit.\nCash out at any time.\nIf you run out of money, you can add more.")
 
-slot_machine_values = ['bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', '7', '7', '7', '7', '7', '7', 'cherry', 'cherry', 'cherry', 'cherry', 'cherry', 'jackpot', 'jackpot', 'jackpot']
+slot_machine_values = ['bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', '7', '7', '7', '7', '7', '7', 'cherry', 'cherry', 'cherry', 'cherry', 'cherry', 'jackpot', 'jackpot', 'jackpot']
 jackpot = ['jackpot', 'jackpot', 'jackpot']
 cherry = ['cherry', 'cherry', 'cherry']
 seven = ['7', '7', '7']
 bar = ['bar', 'bar', 'bar']
 
 def pay_table():
-  print("Welcome to the slot machine! Spin the wheel for your chance to win!\nEach spin costs $0.25.\nThe following combinations will result in a win:\n jackpot, jackpot, jackpot: Pays out the grand jackpot of $10,000!\n cherry, cherry, cherry: Pays $100\n 7, 7, 7: Pays $50\n bar, bar, bar: Pays $20\n 2 cherries: Pays $10\n 1 cherry: Pays $1\nGet ready to play!")
+  print("Welcome to the slot machine! Spin the wheel for your chance to win!\nEach spin costs $0.25.\nThe following combinations will result in a win:\n -jackpot, jackpot, jackpot: Pays out the grand jackpot of $10,000!\n -cherry, cherry, cherry: Pays $100\n -7, 7, 7: Pays $50\n -bar, bar, bar: Pays $20\n -Any combination with 2 cherries: Pays $10\n -Any combination with 1 cherry: Pays $1\nGet ready to play!")
 
 def players_option():
-  play_or_quit = input("Would you to (S)pin, (C)ash out, or (A)dd to your wallet? ").upper()
+  play_or_quit = input("Would you like to (S)pin, (C)ash out, (V)iew your wallet's balance, or (A)dd to your wallet? ").upper()
   if play_or_quit == 'S':
-    cost_of_spin = -0.25
-    my_wallet.append(cost_of_spin)
-    spin()
+    check_wallet_balance()
   elif play_or_quit == 'C':
     print(f"Your wallet's total is ${sum(my_wallet):.2f}. Thanks for visiting the Casino! See you again soon!")
+  elif play_or_quit == 'V':
+    print(f"Your wallet's balance is ${sum(my_wallet):.2f}.")
+    players_option()
   elif play_or_quit == 'A':
     amount = input("How much money would you like to add to your wallet? ")
     my_wallet.append(float(amount))
@@ -29,6 +30,15 @@ def players_option():
     players_option()
   else: 
     print('That is not a valid option. Try again.')
+    players_option()
+
+def check_wallet_balance():
+  if sum(my_wallet) >= 0.25:
+    cost_of_spin = -0.25
+    my_wallet.append(cost_of_spin)
+    spin()
+  else:
+    print("You do not have enough money to play the slot machine. Please add more money to your wallet.")
     players_option()
 
 def spin():
@@ -54,11 +64,35 @@ def spin():
     my_wallet.append(20)
     print("You won $20")
     players_option()
+  elif first_item == 'cherry' and second_item == 'cherry' and third_item != 'cherry':
+    my_wallet.append(10)
+    print("You won $10")
+    players_option()
+  elif first_item == 'cherry' and second_item != 'cherry' and third_item == 'cherry':
+    my_wallet.append(10)
+    print("You won $10")
+    players_option()
+  elif first_item != 'cherry' and second_item == 'cherry' and third_item == 'cherry':
+    my_wallet.append(10)
+    print("You won $10")
+    players_option()
+  elif first_item == 'cherry' and second_item != 'cherry' and third_item != 'cherry':
+    my_wallet.append(1)
+    print("You won $1")
+    players_option()
+  elif first_item != 'cherry' and second_item == 'cherry' and third_item != 'cherry':
+    my_wallet.append(1)
+    print("You won $1")
+    players_option()
+  elif first_item != 'cherry' and second_item != 'cherry' and third_item == 'cherry':
+    my_wallet.append(1)
+    print("You won $1")
+    players_option()  
   else:
     print("You did not win. Sorry.")
     players_option()
-
-
+    
+    
 greeting()
 
 pay_table()
