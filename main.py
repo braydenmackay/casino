f98b121480c2e21dcffead5ebf13190f2e7f3c27
braydenmackay <braydenmackay1@gmail.com>
2019-09-24 14:26:51 -0600

    updated spin function and added cash out function

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import random
+import numpy as np
 
-my_wallet = [2]
+my_wallet = [3]
 
 def greeting():
   print(f"Welcome to the Casino!\nYou have been given a ${sum(my_wallet):.2f} credit.\nCash out at any time.\nIf you run out of money, you can add more.")
 
-slot_machine_values = ['bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bomb', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'bar', '7', '7', '7', '7', '7', '7', 'cherry', 'cherry', 'cherry', 'cherry', 'cherry', 'jackpot', 'jackpot', 'jackpot']
+def pay_table():
+  print("Play the slot machine! Spin the wheel for your chance to win!\nEach spin costs $0.25.\nThe following combinations will result in a win:\n -jackpot, jackpot, jackpot: Pays out the grand jackpot of $10,000!\n -cherry, cherry, cherry: Pays $100\n -7, 7, 7: Pays $50\n -bar, bar, bar: Pays $20\n -Any combination with 2 cherries: Pays $5\n -Any combination with 1 cherry: Pays $1\nGet ready to play!")
+
 jackpot = ['jackpot', 'jackpot', 'jackpot']
 cherry = ['cherry', 'cherry', 'cherry']
 seven = ['7', '7', '7']
 bar = ['bar', 'bar', 'bar']
-
-def pay_table():
-  print("Play the slot machine! Spin the wheel for your chance to win!\nEach spin costs $0.25.\nThe following combinations will result in a win:\n -jackpot, jackpot, jackpot: Pays out the grand jackpot of $10,000!\n -cherry, cherry, cherry: Pays $100\n -7, 7, 7: Pays $50\n -bar, bar, bar: Pays $20\n -Any combination with 2 cherries: Pays $5\n -Any combination with 1 cherry: Pays $0.50\nGet ready to play!")
 
 def players_option():
   play_or_quit = input("Would you like to (S)pin, (C)ash out, (V)iew your wallet's balance, or (A)dd to your wallet? ").upper()
   if play_or_quit == 'S':
     check_wallet_balance()
   elif play_or_quit == 'C':
-    print(f"Your wallet's total is ${sum(my_wallet):.2f}. Thanks for visiting the Casino! See you again soon!")
+    cash_out()
   elif play_or_quit == 'V':
     print(f"Your wallet's balance is ${sum(my_wallet):.2f}.")
     players_option()
@@ -36,7 +35,7 @@ def check_wallet_balance():
     spin()
   else:
     print("You do not have enough money to play the slot machine. Please add more money to your wallet.")
-    players_option()
+    add_money()
 
 def add_money():
   amount = input("How much money would you like to add to your wallet?\n (1)$5.00\n (2)$10.00\n (3)$20.00\n (4)Return to slot machine. ")
@@ -59,9 +58,11 @@ def add_money():
     add_money()
 
 def spin():
-  first_item = random.choice(slot_machine_values)
-  second_item = random.choice(slot_machine_values)
-  third_item = random.choice(slot_machine_values)
+  choices = ['bomb', 'bar', '7', 'cherry', 'jackpot']
+  weights = [0.60, 0.18, 0.15, 0.06, 0.01]
+  first_item = np.random.choice(choices, p=weights)
+  second_item = np.random.choice(choices, p=weights)
+  third_item = np.random.choice(choices, p=weights)
   players_spin = [first_item, second_item, third_item]
   print(f"Your spin is:\n{(', '.join(players_spin))}")
 
@@ -94,20 +95,30 @@ def spin():
     print("You won $5")
     players_option()
   elif first_item == 'cherry' and second_item != 'cherry' and third_item != 'cherry':
-    my_wallet.append(0.50)
-    print("You won $0.50")
+    my_wallet.append(1)
+    print("You won $1.00")
     players_option()
   elif first_item != 'cherry' and second_item == 'cherry' and third_item != 'cherry':
-    my_wallet.append(0.50)
-    print("You won $0.50")
+    my_wallet.append(1)
+    print("You won $1.00")
     players_option()
   elif first_item != 'cherry' and second_item != 'cherry' and third_item == 'cherry':
-    my_wallet.append(0.50)
-    print("You won $0.50")
+    my_wallet.append(1)
+    print("You won $1.00")
     players_option()  
   else:
     print("You did not win. Sorry.")
     players_option()
+
+def cash_out():
+  confirm = input("Are you sure you want to cash out? (Y)es or (N)o ").upper()
+  if confirm == 'Y':
+    print(f"Thank you for coming to the casino today! Your wallet's balance is ${sum(my_wallet):.2f}. See you again soon!")
+  elif confirm == 'N':
+    players_option()
+  else:
+    print("Not a valid selection. Try again.")
+    cash_out()
     
     
 greeting()
